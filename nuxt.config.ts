99729cbe539914f66fb843703df05d87d7ea3430
Дарья Иванова <dashann@mail.ru>
2023-01-13 22:22:42 +0300

    feat/add pinia and element-plus packs, create index page and default layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'LOreal ACD',
+            meta: [
+                {name: 'description', content: 'Medical site'},
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                {name: "format-detection", content: "telephone=no"}
+            ],
+            link: [
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+            ],
+            noscript: [
+                // <noscript>JavaScript is required</noscript>
+                { children: 'JavaScript is required' }
+            ]
+        }
+    },
 
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/assets/styles/colors.scss" as *;'
+                }
+            }
+        }
+    }
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+Hello!
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
